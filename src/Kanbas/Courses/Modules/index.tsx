f1cfb86da0b31f2eb8094b59dc91711c45990a57
Ricky Zang <zhang.rick@northeastern.edu>
2024-10-10 14:29:53 +0300

    style modules screen

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,59 +1,64 @@
+// src/Kanbas/Courses/Modules/index.tsx
+import ModulesControls from "./ModulesControls";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
+import "./style.css"
+
 export default function Modules() {
   return (
     <div>
-      <div className="wd-buttons">
-        <button>Collapse All</button>
-        <button>View Progress</button>
-        <div className="wd-publish-dropdown">
-          <button>Publish All</button>
-          <div className="dropdown-content">
-            <button>Publish All Modules</button>
+      <ModulesControls />
+      <br />
+      <br />
+      <ul id="wd-modules" className="list-group rounded-0">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            Week 1
+            <ModuleControlButtons />
           </div>
-        </div>
-        <button>+ Module</button>
-      </div>{" "}
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-                <li className="wd-content-item">
-                  Understanding HTML, CSS, and JavaScript
-                </li>
-              </ul>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LEARNING OBJECTIVES
+              <LessonControlButtons />
             </li>
-            <li className="wd-lesson">
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Chapter 1: Introduction</li>
-              </ul>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              Introduction to the course
+              <LessonControlButtons />
             </li>
-            <li className="wd-lesson">
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Week 1 Slides</li>
-              </ul>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              Learn what is Web Development
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 1
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 2
+              <LessonControlButtons />
             </li>
           </ul>
         </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Understand HTML Basics</li>
-              </ul>
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            Week 2
+            <ModuleControlButtons />
+          </div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LEARNING OBJECTIVES
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 1
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              LESSON 2
+              <LessonControlButtons />
             </li>
           </ul>
         </li>
-        {/* Add more modules as needed */}
       </ul>
     </div>
   );
