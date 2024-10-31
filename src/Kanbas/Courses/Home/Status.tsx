d7c04178e894ae2b18bb62447c291ce0da766f3b
Rick Zhang <zhang.rick@northeastern.edu>
2024-10-31 07:16:26 +0300

    fix status button icons

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,13 +1,12 @@
 import { RxCircleBackslash } from "react-icons/rx";
-
-
-
-import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaFileImport } from "react-icons/fa";
+import { TbArrowRightToArc } from "react-icons/tb";
+import { GiArrowScope } from "react-icons/gi";
+import { VscGraph } from "react-icons/vsc";
+import { GoMegaphone } from "react-icons/go";
+import { GoBell } from "react-icons/go";
 import { FaCheckCircle, FaCog } from "react-icons/fa";
-import { BiImport } from "react-icons/bi";
-import { LiaFileImportSolid } from "react-icons/lia";
-import { AiOutlineShareAlt } from "react-icons/ai";
-import { FaBullhorn } from "react-icons/fa";
+
 
 export default function CourseStatus() {
   return (
@@ -18,36 +17,36 @@ export default function CourseStatus() {
     >
       <h2 className="fs-5">Course Status</h2>
       <div className="d-flex mb-2">
-        <button className="btn btn-lg btn-secondary me-1">
+        <button className="btn btn-lg btn-secondary me-1 text-start">
           <RxCircleBackslash className="me-2" /> Unpublish
         </button>
-        <button className="btn btn-lg btn-success">
+        <button className="btn btn-lg btn-success text-start">
           <FaCheckCircle className="me-2" /> Published
         </button>
       </div>
       <hr />
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <BiImport className="me-2" /> Import Existing Content
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <FaFileImport className="me-2" /> Import Existing Content
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <LiaFileImportSolid className="me-2" /> Import from Commons
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <TbArrowRightToArc className="me-2" /> Import from Commons
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <FaCog className="me-2" /> Choose Home Page
-      </button>
-
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <AiOutlineShareAlt className="me-2" /> View Course Stream
-      </button>
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <FaBullhorn className="me-2" /> New Announcement
-      </button>
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <FaCheckCircle className="me-2" /> New Analytics
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <GiArrowScope className="me-2" /> Choose Home Page
       </button>
 
-      <button className="btn btn-lg btn-secondary w-100 mb-1">
-        <AiOutlineShareAlt className="me-2" /> View Course Notifications
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <VscGraph className="me-2" /> View Course Stream
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <GoMegaphone className="me-2" /> New Announcement
+      </button>
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <VscGraph className="me-2" /> New Analytics
+      </button>
+
+      <button className="btn btn-lg btn-secondary w-100 mb-1 text-start">
+        <GoBell className="me-2" /> View Course Notifications
       </button>
     </div>
   );
