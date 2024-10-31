b1753a5ab5fc2f4a4c744e9ee750de918dde921e
Rick Zhang <zhang.rick@northeastern.edu>
2024-10-31 13:22:54 +0300

    lab 4 add todo on lab3

---
+++ src/Labs/Lab4/ReduxExamples/todos/todosReducer.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: "1", title: "Learn React" },
+    { id: "2", title: "Learn Node" },
+  ],
+  todo: { title: "Learn Mongo" },
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodos = [
+        // import createSlice
+        // declare initial state of reducer
+        // moved here from TodoList.tsx
+        // todos has default todos
+        // todo has default todo
+        // create slice
+        // name slice
+        // configure store's initial state
+        // declare reducer functions
+        // addTodo reducer function, action
+        // contains new todo. newTodos
+        ...state.todos,
+        { ...action.payload, id: new Date().getTime().toString() },
+      ];
+      state.todos = newTodos;
+      state.todo = { title: "" };
+    },
+    deleteTodo: (state, action) => {
+      const newTodos = state.todos.filter((todo) => todo.id !== action.payload);
+      state.todos = newTodos;
+    },
+    updateTodo: (state, action) => {
+      const newTodos = state.todos.map((item) =>
+        item.id === action.payload.id ? action.payload : item
+      );
+      state.todos = newTodos;
+      state.todo = { title: "" };
+    },
+    setTodo: (state, action) => {
+      state.todo = action.payload;
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, updateTodo, setTodo } = todosSlice.actions;
+export default todosSlice.reducer;
