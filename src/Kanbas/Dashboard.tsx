719f7af8882c5fb82743f9592b80d7851db0a7cf
Rick Zhang <zhang.rick@northeastern.edu>
2024-10-31 10:45:03 +0300

    merge fixed structure from a2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -10,18 +10,36 @@ export default function Dashboard() {
       <hr />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
-      <div className="row g-4">
+      <div
+        id="wd-dashboard-courses"
+        className="row row-cols-1 row-cols-md-5 g-4"
+      >
         {courses.map((course) => (
-          <div key={course._id} className="col-12 col-md-6 col-lg-3">
-            <div className="card">
+          <div
+            className="wd-dashboard-course col"
+            style={{ width: "300px" }}
+            key={course._id}
+          >
+            <div className="wd-dashboard-course-link text-decoration-none text-dark">
               <Link
                 className="wd-dashboard-course-link text-decoration-none"
-                to={`/Kanbas/Courses/${course._id}/Home`}
+                to={`/Kanbas/Courses/${course._id}`}
               >
-                <img src="/images/reactjs.png" width="100%" height={160} />
+                <img
+                  src="/images/reactjs.png"
+                  className="card-img-top"
+                  alt="React JS"
+                  width="100%"
+                  height={160}
+                />
+
                 <div className="card-body">
-                  <h5 className="card-title">{course.name}</h5>
-                  <p className="card-text">{course.description.split(' ').slice(0, 10).join(' ')}</p>
+                  <h5 className="wd-dashboard-course-title card-title">
+                    {course.name}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    {course.description.split(" ").slice(0, 10).join(" ")}
+                  </p>
                   <button className="btn btn-primary">Go</button>
                 </div>
               </Link>
