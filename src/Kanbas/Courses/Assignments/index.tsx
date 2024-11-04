ff6626e7bc6158ff70a774ed273376810e998ca3
Rick Zhang <zhang.rick@northeastern.edu>
2024-11-04 11:06:09 +0300

    assignment list item style

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,6 +4,8 @@ import { FaSearch, FaPlus, FaClipboardList, FaEllipsisV } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteAssignment } from "./reducer";
 import { useState } from "react";
+import { BsGripVertical } from "react-icons/bs";
+import GreenCheckmark from "./GreenCheckmark";
 
 // Define the Assignment interface locally
 interface Assignment {
@@ -36,7 +38,7 @@ export default function Assignments() {
   };
 
   return (
-    <div id="wd-assignments" className="p-4">
+    <div id="wd-assignments">
       <div className="d-flex justify-content-between align-items-center mb-3">
         <div className="input-group w-50">
           <span className="input-group-text">
@@ -63,45 +65,48 @@ export default function Assignments() {
       </div>
 
       <h3 id="wd-assignments-title">
+        <BsGripVertical className="me-2 fs-3" />
         ASSIGNMENTS <span className="text-muted">40% of Total</span>
         <button className="btn btn-secondary float-end">+</button>
       </h3>
 
-      <ul id="wd-assignment-list" className="list-group">
+      <ul id="wd-assignment-list" className="list-group rounded-0">
         {filteredAssignments.length > 0 ? (
           filteredAssignments.map((assignment) => (
             <li
               key={assignment._id}
-              className="list-group-item d-flex justify-content-between align-items-center border-0 mb-2"
+              className="list-group-item d-flex align-items-center border-0 mb-0 p-3 ps-1"
             >
               <div className="d-flex align-items-center">
+              <BsGripVertical className="me-2 fs-3" />
                 <span className="text-success me-2">
                   <FaClipboardList />
                 </span>
-                <span className="text-muted me-2">
-                  <FaEllipsisV />
-                </span>
+                
               </div>
 
-              <div>
+
+              <div className="ms-2">
                 <Link
-                  className="wd-assignment-link me-2"
+                  className="wd-assignment-link me-2 text-dark fw-bold fs-5 text-decoration-none"
                   to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
                 >
                   {assignment.title}
                 </Link>
 
                 <br />
-                <span className="text-danger">| Multiple Modules</span>
+                <span className="text-danger"> Multiple Modules</span>
 
-                <span>
-                  | Not available until {assignment.availableFrom} | Due {assignment.dueDate} | {assignment.points} pts
+                <span className="ms-1">
+                  | Not available until {assignment.availableFrom}  Due{" "}
+                  {assignment.dueDate} | {assignment.points} pts
                 </span>
               </div>
-
-              <span className="text-muted me-2">
-                <FaEllipsisV />
-              </span>
+              
+              <div className="text-muted me-2 float-end ms-auto p-2">
+              <GreenCheckmark />
+                <FaEllipsisV className="ms-3"/>
+              </div>
             </li>
           ))
         ) : (
